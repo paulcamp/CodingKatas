a7880f4569ff67d23bc64fddba4e05ada2777dd8
Paul <paul.campbell.nottingham@gmail.com>
2019-07-19 20:47:35 +0100

    Credit Card Test (Luhn) Exercise

---
+++ CreditCardTest/LuhnTest/LuhnTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace LuhnTest
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: allow command line stdout based running
+
+            Console.Write("Luhn Check of 49927398716, result "); Console.Write(LuhnCheck("49927398716") == true ? "Valid" : "Invalid"); Console.WriteLine();
+            Console.Write("Luhn Check of 49927398717, result "); Console.Write(LuhnCheck("49927398717") == true ? "Valid" : "Invalid"); Console.WriteLine();
+            Console.Write("Luhn Check of 1234567812345678, result "); Console.Write(LuhnCheck("1234567812345678") == true ? "Valid" : "Invalid"); Console.WriteLine();
+            Console.Write("Luhn Check of 1234567812345670, result "); Console.Write(LuhnCheck("1234567812345670") == true ? "Valid" : "Invalid"); Console.WriteLine();
+            Console.ReadKey();  
+        }
+
+
+        static bool LuhnCheck(string cardDigits)
+        {
+            string reversed = Reverse(cardDigits);
+
+            int[] numbers = reversed.Select(c => c - '0').ToArray();
+
+            bool oddState = true;
+            int len = numbers.Length;
+            int index = 0;
+            int s1 = 0;
+            int s2 = 0;
+
+            do
+            {
+                if(oddState)
+                {
+                    s1 += numbers[index];
+                }
+                else
+                {
+                    var partialS2 = numbers[index] * 2;
+
+                    if (partialS2 > 9)
+                        partialS2 = partialS2 - 9; //same as adding each digit (eg 12 => 1+2 = 3, same as 12 - 9)
+
+                    s2 += partialS2;
+                }
+
+                oddState = !oddState;
+                index++;
+
+            } while (index < len);
+
+            
+            var sum = s1 + s2;
+
+            if (sum % 10 == 0)
+                return true;
+
+            return false;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+              
+    }
+}
--- README.md
+++ README.md
@@ -1,15 +1,55 @@
 # CodingKatas
 
 
-## C# Implementation of [supermarket kata](http://codekata.com/kata/kata01-supermarket-pricing/)   
+### [Supermarket kata](http://codekata.com/kata/kata01-supermarket-pricing/)   
+See link for more details
 
-Pre-Reqs:
+### [Binary Chop](http://codekata.com/kata/kata02-karate-chop/)
 
-Running the Tests: 
+See Link for more details
 
-Approach: 
+### Credit Card Test
 
-Areas for improvement:
+The Luhn test is used by some credit card companies to distinguish valid credit card numbers from what could be a random selection of digits.
+
+Those companies using credit card numbers that can be validated by the Luhn test have numbers that pass the following test:
+
+* Reverse the order of the digits in the number.
+* Take the first, third, ... and every other odd digit in the reversed digits and sum them to form the partial sum s1   
+* Taking the second, fourth ... and every other even digit in the reversed digits:
+* Multiply each digit by two and sum the digits if the answer is greater than nine to form partial sums for the even digits
+* Sum the partial sums of the even digits to form s2
+* If s1 + s2 ends in zero then the original number is in the form of a valid credit card number as verified by the Luhn test.
+
+For example, if the trial number is 49927398716:
+
+Reverse the digits:  
+  61789372994  
+Sum the odd digits:  
+  6 + 7 + 9 + 7 + 9 + 4 = 42 = s1  
+The even digits:  
+    1,  8,  3,  2,  9  
+  Two times each even digit:  
+    2, 16,  6,  4, 18  
+  Sum the digits of each multiplication:  
+    2,  7,  6,  4,  9  
+  Sum the last:  
+    2 + 7 + 6 + 4 + 9 = 28 = s2  
+
+s1 + s2 = 70 which ends in zero which means that 49927398716 passes the Luhn test
+
+#### Task
+Write a function/method/procedure/subroutine that will validate a number with the Luhn test, and 
+use it to validate the following numbers:
+
+   49927398716  
+   49927398717  
+   1234567812345678  
+   1234567812345670  
+
+
+
+
 
 
 
