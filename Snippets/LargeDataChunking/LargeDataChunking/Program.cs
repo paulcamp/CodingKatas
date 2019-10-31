b2334ba771cb9ad7c3518d8d7f5692c35bedfcaf
Paul Campbell <paul.campbell@myunidays.com>
2019-10-31 16:08:28 +0000

    data chunking example and SQL trigger snippet

---
+++ Snippets/LargeDataChunking/LargeDataChunking/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace LargeDataChunking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //stream large data table out to file
+            StreamWriter sw = new StreamWriter(@"d:\somedatafile.txt");
+
+            foreach (var block in GetData())
+            {
+                sw.WriteLine(block);
+            }
+
+            sw.Close();
+        }
+
+
+        public static  IEnumerable<string> GetData()
+        {
+            string connectionString = @"Data Source=.;Initial Catalog=Main;Integrated Security=True;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"Select Id from dbo.YOURTABLE";
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string foo = reader.GetGuid(0).ToString();
+                            yield return foo;
+                    }
+                }
+            }
+        }
+
+    }
+}
