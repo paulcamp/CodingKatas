4876cefd758c91b5589ba17d2bed630ad502eb36
Paul Campbell <paul.campbell@myunidays.com>
2019-11-05 15:55:46 +0000

    forgot the actual batch!

--- Snippets/LargeDataChunking/LargeDataChunking/Program.cs
+++ Snippets/LargeDataChunking/LargeDataChunking/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.IO;
+using MoreLinq;
 
 namespace LargeDataChunking
 {
@@ -8,19 +10,22 @@ namespace LargeDataChunking
     {
         static void Main(string[] args)
         {
-            //stream large data table out to file
-            StreamWriter sw = new StreamWriter(@"d:\somedatafile.txt");
+            //Option1 (uncomment)
+            //GetAllData(); //922Mb about 1 minute
 
-            foreach (var block in GetData())
+            //Option2
+            //stream large data table out to file
+            StreamWriter sw = new StreamWriter(@"d:\some-streamed-datafile.txt");
+
+            foreach (var batch in StreamData().Batch(1000))
             {
-                sw.WriteLine(block);
+                sw.WriteLine(batch);
             }
 
             sw.Close();
         }
 
-
-        public static  IEnumerable<string> GetData()
+        public static void GetAllData()
         {
             string connectionString = @"Data Source=.;Initial Catalog=Main;Integrated Security=True;";
 
@@ -28,14 +33,53 @@ namespace LargeDataChunking
             {
                 connection.Open();
                 var cmd = connection.CreateCommand();
-                cmd.CommandText = @"Select Id from dbo.YOURTABLE";
+                cmd.CommandText = @"SELECT [PerkId]
+      ,[StudentId]
+      ,[Site]
+      ,[UsedOn]
+      ,[UsedOnHour]
+      ,[UsedOnDay]
+      ,[InferredUse]
+  FROM [Main].[dbo].[YOURTABLE] ORDER BY UsedOn";
+
+                var sda = new SqlDataAdapter(cmd);
+                var dataSet = new DataSet();
+                var numResults = sda.Fill(dataSet);
+
+
+                StreamWriter sw = new StreamWriter(@"d:\some-big-datafile.txt");
+                dataSet.WriteXml(sw);
+                sw.Close();
+            
+            }
+        }
+
+
+        public static  IEnumerable<string> StreamData()
+        {
+            string connectionString = @"Data Source=.;Initial Catalog=Main;Integrated Security=True;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"SELECT [PerkId]
+      ,[StudentId]
+      ,[Site]
+      ,[UsedOn]
+      ,[UsedOnHour]
+      ,[UsedOnDay]
+      ,[InferredUse]
+  FROM [Main].[dbo].[YOURTABLE] ORDER BY UsedOn";
 
                 using (var reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
                     {
                         string foo = reader.GetGuid(0).ToString();
-                            yield return foo;
+                        string bar = reader.GetGuid(1).ToString();
+                        string cat = reader.GetString(2);
+                        yield return foo + '|' + bar + '|' + cat;
                     }
                 }
             }
